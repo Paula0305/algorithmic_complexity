2edb596c9872e648fd82cdcc3bbbd7c4f74bf505
Paulina Pasternak <117033508+Paula0305@users.noreply.github.com>
2022-11-09 17:09:09 +0100

    Add files via upload

---
+++ sieve_Eratosthenes.py
@@ -0,0 +1,26 @@
+from math import sqrt
+
+
+def sieve_Eratosthenes(x):
+    primes = []
+    count = 0
+    primes = [True] * (x + 1)  
+    for i in range(2, x + 1):
+        if primes[i]:
+            for j in range(i + i, x + 1, i):
+                primes[j] = False
+
+    for i in range(2, x + 1):
+        if primes[i]:
+            print(i)
+            count += 1
+
+    return print(f"The number of primes in a given range is: {count}")
+
+
+x = int(input("Enter a positive number greater than 2:"))
+while x <= 2:
+    x = int(input("Enter positiv number:"))
+    
+sieve_Eratosthenes(x)
+
